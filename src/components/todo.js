ae245c84ac292cf82aab71f518640985996c2771
Chaoyang Jia <cyjia@thoughtworks.com>
2016-06-08 11:55:26 +0800

    refactor

--- src/components/todo.js
+++ src/components/todo.js
@@ -5,23 +5,14 @@ import CompleteTodo from './complete-todo';
 export default class Todo extends React.Component {
   render() {
     const todo = this.props.todo;
-    return todo.completed ? (
-      <li className="completed">
+    return (
+      <li className={`${todo.completed ? 'completed' : ''}`}>
         <div className="view">
           <CompleteTodo todo={todo}/>
           <label>{todo.title}</label>
           <DeleteTodo todo={todo}/>
         </div>
-        <input className="edit" value="Create a TodoMVC template"/>
-      </li>) :
-      (<li>
-          <div className="view">
-            <CompleteTodo todo={todo}/>
-            <label>{todo.title}</label>
-            <DeleteTodo todo={todo}/>
-          </div>
-          <input className="edit" value="Rule the web"/>
-        </li>
-      );
+        <input className="edit" value={todo.title}/>
+      </li>)
   }
 }
